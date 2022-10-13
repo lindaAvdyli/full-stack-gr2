6d4879dcf67e01084740ff2800985180499a2d23
sididev96 <sejjid.gashi@gmail.com>
2022-10-13 18:54:05 +0200

    detyrat

--- detyrat/2.js
+++ detyrat/2.js
@@ -3,7 +3,16 @@
 // Your response should be a number (length of a largest string).
 
 function findLongestWordLength(str) {
-
+  var strSplit = str.split(" ");
+  var longestWord = 0;
+  for (i = 0; i < strSplit.length; i++) {
+    if (strSplit[i].length > longestWord) {
+      longestWord = strSplit[i].length;
+    }
+  }
+  return longestWord;
 }
-  
-findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
+console.log(
+  findLongestWordLength("The quick brown fox jumped over the lazy dog")
+);
--- detyrat/3.js
+++ detyrat/3.js
@@ -4,7 +4,21 @@
 // Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
 
 function largestOfFour(arr) {
-
+  largestNums = [0, 0, 0, 0];
+  for (i = 0; i < arr.length; i++) {
+    for (x = 0; x < arr[i].length; x++) {
+      if (arr[i][x] > largestNums[i]) {
+        largestNums[i] = arr[i][x];
+      }
+    }
+  }
+  return largestNums;
 }
-  
-largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+console.log(
+  largestOfFour([
+    [4, 5, 1, 3],
+    [13, 27, 18, 26],
+    [32, 35, 37, 39],
+    [1000, 1001, 857, 1],
+  ])
+);
