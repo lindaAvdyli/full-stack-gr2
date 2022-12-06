97874e66a73c17ba9aa1a77dc989ade6d99ad876
ShaniGurizi03 <shannche4@gmail.com>
2022-12-06 18:50:03 +0100

    my message

--- database/main.js
+++ database/main.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const db = require('./database/database.js')
+const db = require('./database.js')
 
 app.use(express.json())
 
@@ -76,6 +76,46 @@ app.get('/users/limit/:page',(req,res)=>{
     db.query(`SELECT * FROM subjects LIMIT ${page},10`, (err,result)=>{
         if(err){ throw err}
         res.json(result)
+    })
+})
+
+app.delete('/users/:id',(req,res)=>{
+    db.query('DELETE FROM students WHERE id='+req.params.id, (err,result)=>{
+        if(err){ throw err}
+        res.json(result)
+    })
+})
+app.post('/users',(req,res)=>{
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+    db.query('INSERT INTO students(firstName,lastName,email) VALUES ("'+firstName+'","'+lastName+'","'+email+'")'
+    , (err,result)=>{
+        if(err){ throw err}
+        res.json(result)
+    })
+})
+
+app.put('/users/:id',(req,res)=>{
+
+    const firstName = req.body.firstName
+    const lastName = req.body.lastName
+    const userId = req.params.id
+
+    const getUser = `Select count(id) as count FROM students where id = '${userId}'`
+    const sql = `UPDATE students SET firstName = '${firstName}', lastName = '${lastName}' WHERE (id = '${userId}')`;
+
+    db.query(getUser,(err,result)=>{
+        if(err){ throw err}
+        if(result[0].count !== 0){
+            db.query(sql ,(err,result)=>{
+                if(err){ throw err}
+                res.json({message:`Student with id ${userId} has been updated!`})
+            })
+        }else{
+            res.json({message:`Student with id ${userId} is not found!`}, 404)
+        }
+
     })
 })
 
